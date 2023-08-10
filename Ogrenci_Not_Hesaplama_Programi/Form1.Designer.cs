1f6495a419cc490d1232e809e23188866163d5d4
Mustafa Vural <vuralmustafa474@gmail.com>
2023-08-11 02:56:40 +0300

    ogrenci_not_hesaplama_masaustu_uygulama
    C# kullanarak öğrenci not hesaplama

---
+++ Ogrenci_Not_Hesaplama_Programi/Form1.Designer.cs
@@ -0,0 +1,358 @@
+﻿namespace Ogrenci_Not_Hesaplama_Programi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label18 = new System.Windows.Forms.Label();
+            this.label19 = new System.Windows.Forms.Label();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.textBox12 = new System.Windows.Forms.TextBox();
+            this.textBox13 = new System.Windows.Forms.TextBox();
+            this.textBox14 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label17 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label20 = new System.Windows.Forms.Label();
+            this.label21 = new System.Windows.Forms.Label();
+            this.label22 = new System.Windows.Forms.Label();
+            this.groupBox2.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label22);
+            this.groupBox2.Controls.Add(this.label21);
+            this.groupBox2.Controls.Add(this.textBox14);
+            this.groupBox2.Controls.Add(this.textBox13);
+            this.groupBox2.Controls.Add(this.textBox12);
+            this.groupBox2.Controls.Add(this.textBox11);
+            this.groupBox2.Controls.Add(this.textBox10);
+            this.groupBox2.Controls.Add(this.textBox9);
+            this.groupBox2.Controls.Add(this.textBox8);
+            this.groupBox2.Controls.Add(this.label19);
+            this.groupBox2.Controls.Add(this.label18);
+            this.groupBox2.Controls.Add(this.label16);
+            this.groupBox2.Controls.Add(this.label15);
+            this.groupBox2.Controls.Add(this.label14);
+            this.groupBox2.Controls.Add(this.label13);
+            this.groupBox2.Controls.Add(this.label12);
+            this.groupBox2.Location = new System.Drawing.Point(12, 214);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(442, 397);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 27;
+            this.listBox2.Location = new System.Drawing.Point(450, 248);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(760, 328);
+            this.listBox2.TabIndex = 1;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.label12.Location = new System.Drawing.Point(49, 66);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(60, 27);
+            this.label12.TabIndex = 0;
+            this.label12.Text = "ADI:";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.label13.Location = new System.Drawing.Point(6, 115);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(103, 27);
+            this.label13.TabIndex = 1;
+            this.label13.Text = "SOYADI:";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.label14.Location = new System.Drawing.Point(38, 161);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(71, 27);
+            this.label14.TabIndex = 2;
+            this.label14.Text = "VİZE:";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.label15.Location = new System.Drawing.Point(21, 206);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(88, 27);
+            this.label15.TabIndex = 3;
+            this.label15.Text = "FİNAL:";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.label16.Location = new System.Drawing.Point(46, 248);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(63, 27);
+            this.label16.TabIndex = 4;
+            this.label16.Text = "BÜT:";
+            // 
+            // label18
+            // 
+            this.label18.AutoSize = true;
+            this.label18.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.label18.Location = new System.Drawing.Point(21, 335);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(212, 27);
+            this.label18.TabIndex = 6;
+            this.label18.Text = "YENİ ORTALAMA:";
+            // 
+            // label19
+            // 
+            this.label19.AutoSize = true;
+            this.label19.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.label19.Location = new System.Drawing.Point(21, 292);
+            this.label19.Name = "label19";
+            this.label19.Size = new System.Drawing.Size(149, 27);
+            this.label19.TabIndex = 7;
+            this.label19.Text = "ORTALAMA:";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(111, 63);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(197, 35);
+            this.textBox8.TabIndex = 8;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(111, 107);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(197, 35);
+            this.textBox9.TabIndex = 9;
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(111, 236);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(197, 35);
+            this.textBox10.TabIndex = 10;
+            // 
+            // textBox11
+            // 
+            this.textBox11.Location = new System.Drawing.Point(111, 158);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(197, 35);
+            this.textBox11.TabIndex = 11;
+            // 
+            // textBox12
+            // 
+            this.textBox12.Location = new System.Drawing.Point(111, 198);
+            this.textBox12.Name = "textBox12";
+            this.textBox12.Size = new System.Drawing.Size(197, 35);
+            this.textBox12.TabIndex = 12;
+            // 
+            // textBox13
+            // 
+            this.textBox13.Location = new System.Drawing.Point(176, 284);
+            this.textBox13.Name = "textBox13";
+            this.textBox13.Size = new System.Drawing.Size(197, 35);
+            this.textBox13.TabIndex = 13;
+            // 
+            // textBox14
+            // 
+            this.textBox14.Location = new System.Drawing.Point(230, 332);
+            this.textBox14.Name = "textBox14";
+            this.textBox14.Size = new System.Drawing.Size(153, 35);
+            this.textBox14.TabIndex = 14;
+            // 
+            // button2
+            // 
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(591, 588);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(189, 41);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Notları Aktar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.label17.Location = new System.Drawing.Point(946, 588);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(82, 27);
+            this.label17.TabIndex = 3;
+            this.label17.Text = "label17";
+            this.label17.Visible = false;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label20);
+            this.panel2.Controls.Add(this.pictureBox2);
+            this.panel2.Location = new System.Drawing.Point(7, 1);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1203, 226);
+            this.panel2.TabIndex = 4;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(576, -11);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(408, 237);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label20
+            // 
+            this.label20.AutoSize = true;
+            this.label20.BackColor = System.Drawing.Color.Aqua;
+            this.label20.Font = new System.Drawing.Font("Swis721 BlkEx BT", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label20.ForeColor = System.Drawing.Color.Black;
+            this.label20.Location = new System.Drawing.Point(75, 116);
+            this.label20.Name = "label20";
+            this.label20.Size = new System.Drawing.Size(462, 35);
+            this.label20.TabIndex = 1;
+            this.label20.Text = "ÖĞRENCİ NOT BİLGİSİ";
+            // 
+            // label21
+            // 
+            this.label21.AutoSize = true;
+            this.label21.ForeColor = System.Drawing.Color.Black;
+            this.label21.Location = new System.Drawing.Point(331, 71);
+            this.label21.Name = "label21";
+            this.label21.Size = new System.Drawing.Size(82, 27);
+            this.label21.TabIndex = 15;
+            this.label21.Text = "label21";
+            // 
+            // label22
+            // 
+            this.label22.AutoSize = true;
+            this.label22.ForeColor = System.Drawing.Color.Black;
+            this.label22.Location = new System.Drawing.Point(331, 115);
+            this.label22.Name = "label22";
+            this.label22.Size = new System.Drawing.Size(82, 27);
+            this.label22.TabIndex = 16;
+            this.label22.Text = "label22";
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.Color.Aqua;
+            this.ClientSize = new System.Drawing.Size(1203, 656);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.label17);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.groupBox2);
+            this.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.ForeColor = System.Drawing.Color.Aqua;
+            this.Name = "Form1";
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBox14;
+        private System.Windows.Forms.TextBox textBox13;
+        private System.Windows.Forms.TextBox textBox12;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Label label19;
+        private System.Windows.Forms.Label label18;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label20;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label22;
+        private System.Windows.Forms.Label label21;
+    }
+}
+
---
+++ Ogrenci_Not_Hesaplama_Programi/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ogrenci_Not_Hesaplama_Programi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double ort, but, ort2;
+            int vize, final;
+            String adi, soyadi;
+            adi = textBox8.Text;
+            label21.Text = adi;
+            soyadi = textBox9.Text;
+            label22.Text = soyadi;
+            vize = Convert.ToInt16(textBox11.Text);
+            final = Convert.ToInt16(textBox12.Text);
+
+            ort = vize * 0.4 + final * 0.6;
+
+            textBox13.Text = ort.ToString();
+            listBox2.Items.Add("ADI:" + textBox8.Text);
+            listBox2.Items.Add("SOYADİ:" + textBox9.Text);
+            listBox2.Items.Add("Vize Notu:" + textBox11.Text);
+            listBox2.Items.Add("Final Notu:" + textBox12.Text);
+            listBox2.Items.Add("Ortalama:" + textBox13.Text);
+
+            if (ort >= 0 && ort < 20)
+            {
+                label17.Text = "Dersten kaldiniz.Harf Notunuz FF.Büte girmeniz gerekiyor.";
+                listBox2.Items.Add("Gecdi/Kaldi:" + label17.Text);
+
+            }
+            else if (ort >= 20 && ort < 30)
+            {
+                label17.Text = "Dersten kaldiniz.Harf Notunuz FD.Büte girmeniz gerekiyor.";
+                listBox2.Items.Add("Gecdi/Kaldi:" + label17.Text);
+            }
+            else if (ort >= 30 && ort < 40)
+            {
+                label17.Text = "Dersten kaldiniz.Harf Notunuz DD.Büte girmeniz gerekiyor.";
+                listBox2.Items.Add("Gecdi/Kaldi:" + label17.Text);
+            }
+            else if (ort >= 40 && ort <= 45)
+            {
+                label17.Text = "Dersten kosullu gectiniz.Harf Notunuz DC.Büte girebilirsiniz. İsteğe bağlı.";
+                listBox2.Items.Add("Gecdi/Kaldi:" + label17.Text);
+            }
+            else if (ort > 45 && ort < 60)
+            {
+                label17.Text = "Dersten  gectiniz.Harf Notunuz CC.Büte girebilirsiniz. İsteğe bağlı.";
+                listBox2.Items.Add("Gecdi/Kaldi:" + label17.Text);
+            }
+
+            else if (ort >= 60 && ort < 70)
+            {
+                label17.Text = "Dersten gectiniz.Harf Notunuz CB.Büte girebilirsiniz. İsteğe bağlı.";
+                listBox2.Items.Add("Gecdi/Kaldi:" + label17.Text);
+            }
+
+            else if (ort >= 70 && ort < 80)
+            {
+                label17.Text = "Dersten gectiniz.Harf Notunuz BB.Büte girebilirsiniz. İsteğe bağlı.";
+                listBox2.Items.Add("Gecdi/Kaldi:" + label17.Text);
+            }
+
+            else if (ort >= 80 && ort < 90)
+            {
+                label17.Text = "Dersten gectiniz.Harf Notunuz AB.Büte girebilirsiniz. İsteğe bağlı.";
+                listBox2.Items.Add("Gecdi/Kaldi:" + label17.Text);
+            }
+
+            else if (ort >= 90 && ort < 100)
+            {
+                label17.Text = "Dersten gectiniz.Harf Notunuz AA.Büte girebilirsiniz. İsteğe bağlı.";
+                listBox2.Items.Add("Gecdi/Kaldi:" + label17.Text);
+            }
+            else
+            {
+                MessageBox.Show("Not Degerleri hatali tekrar giriniz.ortalama hesaplanamadi");
+            }
+        }
+    }
+    }
+
+
+
+
